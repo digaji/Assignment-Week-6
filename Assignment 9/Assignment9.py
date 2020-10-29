d38e9547be475c0151388082c3a90e2ad29ebb87
Jason J. W <71363607+digaji@users.noreply.github.com>
2020-10-29 14:38:46 +0700

    Updated assignment 9 with slightly updated regex

--- Assignment 9/Assignment9.py
+++ Assignment 9/Assignment9.py
@@ -4,6 +4,7 @@ def sep_lines(filename):
     with open(filename, "r", encoding="utf-8") as f:
         with open(f"line_{filename}", "w", encoding="utf-8") as new:
             file = f.read()
-            new.write(re.sub(r'(?<=[^A-Z].[.?]) +(?=[A-Z])', "\n", file))
+            # Writes over the new text file with the old text, but replacing whitespace that meets the conditions with regex
+            new.write(re.sub(r"(?<=[^A-Z].[.?]) +(?=[A-Z\"])", "\n", file))
 
 sep_lines("shorttext.txt")
