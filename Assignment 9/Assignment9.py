af8e13f9ba0eaef96144386ecf7679d3861a809d
Jason J. W <71363607+digaji@users.noreply.github.com>
2020-10-29 14:44:12 +0700

    Changed to import only sub from regex

--- Assignment 9/Assignment9.py
+++ Assignment 9/Assignment9.py
@@ -1,10 +1,10 @@
-import re
+from re import sub
 
 def sep_lines(filename):
     with open(filename, "r", encoding="utf-8") as f:
         with open(f"line_{filename}", "w", encoding="utf-8") as new:
             file = f.read()
             # Writes over the new text file with the old text, but replacing whitespace that meets the conditions with regex
-            new.write(re.sub(r"(?<=[^A-Z].[.?]) +(?=[A-Z\"])", "\n", file))
+            new.write(sub(r"(?<=[^A-Z].[.?]) +(?=[A-Z\"])", "\n", file))
 
 sep_lines("shorttext.txt")
